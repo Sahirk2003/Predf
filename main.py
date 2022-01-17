53346d810f966eb9b1da9a5b5eca1277d2dfdfd7
Sahirk2003 <54923967+Sahirk2003@users.noreply.github.com>
2022-01-17 05:12:34 -0500

    Update main.py
    added modules and config.py, replaced references to direct path with relative paths using os

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-
-
+import nextcord
+from pdf2image import convert_from_path
+import aiofiles
+import aiohttp
 import os
 import warnings
 from PIL import Image
-
+from Private.config import TOKEN
 
 warnings.simplefilter ('ignore', Image.DecompressionBombWarning)
 print ('start')
-TOKEN = 
 dir = os.path.dirname(__file__)
 
-attachment = os.path.join(dir, r'attachment_file.pdf')
+attachment_file = os.path.join(dir, r'attachment_file.pdf')
+print (attachment_file)
 converted_pdfs = os.path.join(dir, r'ConvertedPDFs')
-client = discord.Client()
+print(converted_pdfs)
+client = nextcord.Client()
 
 @client.event
 async def on_ready():
@@ -34,14 +37,16 @@ async def on_message(message):
                     url = attachment.url
                     async with session.get(url) as resp:
                         if resp.status ==200:
-                            f = await aiofiles.open(attachment, mode ='wb')
+                            f = await aiofiles.open(attachment_file, mode ='wb')
                             await f.write(await resp.read())
                             f.close
 
-                convert_from_path(attachment, dpi=500, output_folder=converted_pdfs, fmt ="jpeg", size =(1000,None), poppler_path = r"C:\Users\Sahir\Downloads\Release-21.11.0-0\poppler-21.11.0\Library\bin", paths_only= True)
+                convert_from_path(attachment_file, dpi=500, output_folder=converted_pdfs, fmt ="jpeg", size =(1000,None), paths_only= True)
+                os.remove(attachment_file)
                 for image_path in os.listdir(converted_pdfs):
                     full_path = os.path.join(converted_pdfs,image_path)
-                    await message.channel.send(file=discord.File(full_path))
+                    await message.channel.send(file=nextcord.File(full_path))
                     os.remove(full_path)
+                
        
 client.run(TOKEN)  
