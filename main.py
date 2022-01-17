2a16ee62ab45da2201a5d31948b97571877a1978
Sahirk2003 <54923967+Sahirk2003@users.noreply.github.com>
2022-01-17 11:39:38 -0500

    Update main.py
    added functionality with threads

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 import nextcord
 from pdf2image import convert_from_path
-import aiofiles
 import aiohttp
 import os
 import warnings
 from PIL import Image
+import random
 from Private.config import TOKEN
 
 warnings.simplefilter ('ignore', Image.DecompressionBombWarning)
 print ('start')
 dir = os.path.dirname(__file__)
-
-attachment_file = os.path.join(dir, r'attachment_file.pdf')
-print (attachment_file)
-converted_pdfs = os.path.join(dir, r'ConvertedPDFs')
-print(converted_pdfs)
 client = nextcord.Client()
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -28,25 +25,36 @@ async def on_message(message):
         return
     
     channel = str(message.channel.name)
-    
+   
+
     if message.attachments:
+
         for attachment in message.attachments:
+
+            attachment_file = os.path.join(dir, r'attachment_file{n}.pdf'.format(n = random.randint(1,100000)))
+            converted_pdfs = os.path.join(dir, r'ConvertedPDFs{i}'.format(i = random.randint(1,100000)))
+            os.mkdir(converted_pdfs)
+
             if 'pdf' in attachment.content_type:
+                THREAD = await message.create_thread(name = 'Feedback for {nameu}'.format (nameu = str(message.author).split('#')[0]))
+
                 #await message.channel.send(attachment.url)
                 async with aiohttp.ClientSession() as session:
                     url = attachment.url
                     async with session.get(url) as resp:
                         if resp.status ==200:
-                            f = await aiofiles.open(attachment_file, mode ='wb')
-                            await f.write(await resp.read())
-                            f.close
+                            with open(attachment_file, mode ='wb') as f:
+                                f.write(await resp.read())
+                            
 
-                convert_from_path(attachment_file, dpi=500, output_folder=converted_pdfs, fmt ="jpeg", size =(1000,None), paths_only= True)
+                convert_from_path(attachment_file, dpi=500, output_folder=converted_pdfs, fmt ="jpeg", size =(1000,None), poppler_path=r"C:\Users\Sahir\Downloads\Release-21.11.0-0\poppler-21.11.0\Library\bin", paths_only= True)
                 os.remove(attachment_file)
                 for image_path in os.listdir(converted_pdfs):
                     full_path = os.path.join(converted_pdfs,image_path)
-                    await message.channel.send(file=nextcord.File(full_path))
+                    await THREAD.send(file=nextcord.File(full_path))
                     os.remove(full_path)
+
+                os.rmdir(converted_pdfs)
                 
        
 client.run(TOKEN)  
